60c52fcf991f17da23851c19395bda46e3b90d39
sabuj-tech <info.aboutsa21@gmail.com>
2025-07-13 16:42:37 +0600

    my SQL

---
+++ student.sql
@@ -0,0 +1,50 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 13, 2025 at 12:36 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `student`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student_information`
+--
+
+CREATE TABLE `student_information` (
+  `Roll` int(11) DEFAULT NULL,
+  `Name` varchar(10) DEFAULT NULL,
+  `Gender` varchar(10) DEFAULT NULL,
+  `Age` int(4) DEFAULT NULL,
+  `Gpa` int(10) DEFAULT NULL,
+  `City` varchar(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `student_information`
+--
+
+INSERT INTO `student_information` (`Roll`, `Name`, `Gender`, `Age`, `Gpa`, `City`) VALUES
+(101, 'Rahim', 'Male', 18, 3, 'sylhet'),
+(102, 'Hasina', 'Female', 17, 5, 'Dhaka');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
